d1dedaa9b5b6c9f7d2230d40147d6448b1606ef5
Daeki <kimdaeki@gmail.com>
2018-12-16 12:42:00 +0900

    메인액티비티 xml 수정 / 자바코드 + 베이스액티비티 생성

---
+++ app/src/main/java/kr/tjeit/a20181216_baemincopy/BaseActivity.java
@@ -0,0 +1,11 @@
+package kr.tjeit.a20181216_baemincopy;
+
+import android.support.v7.app.AppCompatActivity;
+
+public abstract class BaseActivity extends AppCompatActivity {
+
+    public abstract void setupEvents();
+    public abstract void setValues();
+    public abstract void bindViews();
+
+}
--- app/src/main/java/kr/tjeit/a20181216_baemincopy/MainActivity.java
+++ app/src/main/java/kr/tjeit/a20181216_baemincopy/MainActivity.java
@@ -2,12 +2,78 @@ package kr.tjeit.a20181216_baemincopy;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends BaseActivity {
+
+    LinearLayout koreanFoodBtn;
+    LinearLayout bunsikBtn;
+    LinearLayout japanesFoodBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        bindViews();
+        setupEvents();
+        setValues();
+
+
+
+
+
+
+    }
+
+
+    @Override
+    public void setupEvents() {
+
+//        한식 버튼이 눌렸을때 할일.
+        koreanFoodBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "한식을 눌렀습니다.", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        bunsikBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "분식을 눌렀습니다.", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        japanesFoodBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "돈까스.회.일식을 눌렀습니다.", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    @Override
+    public void setValues() {
+
+    }
+
+    @Override
+    public void bindViews() {
+
+//        사실 읽을 필요가 전혀 없는 코드들.
+//        따로 메쏘드를 만들어서 밑으로 치워버리자.
+        koreanFoodBtn = findViewById(R.id.koreanFoodBtn);
+        bunsikBtn = findViewById(R.id.bunsikBtn);
+        japanesFoodBtn = findViewById(R.id.japanesFoodBtn);
     }
 }
+
+
+
+
+
+
+
+
