6e98bcdecabd90348df47f687240ae0d5c2c191c
Daeki <kimdaeki@gmail.com>
2018-12-16 12:18:29 +0900

    Initial commit

---
+++ app/src/main/java/kr/tjeit/a20181216_baemincopy/MainActivity.java
@@ -0,0 +1,13 @@
+package kr.tjeit.a20181216_baemincopy;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
